a25d22565fedcfd5be93f8553267edda3d67373f
leonekmi <leonekmi@hotmail.com>
2017-12-03 00:38:17 +0100

    1.9.4 (for Chrome)
    Changed the updater using update_url (i do for Firefox just after)

--- updater.js
+++ updater.js
@@ -1,5 +1,6 @@
 // Update Checker
-var version = "1.9.1\n";
+// This updater is discontinued, it will be deleted in 1.10
+/*var version = "1.9.4\n";
 $.get( {url: "https://leonekmi.twittolabel.fr/anilist-scrobble/version.txt", cache: false} , function (data) {
     console.log("Version " + data);
     if (version != data) {
@@ -17,5 +18,27 @@ $.get( {url: "https://leonekmi.twittolabel.fr/anilist-scrobble/version.txt", cac
         });
     } else {
         console.log("No update available");
+    }
+});*/
+chrome.runtime.onInstalled.addListener(function(details){
+    if(details.reason == "install"){
+        console.log("This is a first install!");
+        var options = {
+            type: "basic",
+            iconUrl: "https://leonekmi.twittolabel.fr/anilist-scrobble/login.png",
+            title: "Anilist Scrobbler",
+            message: chrome.i18n.getMessage('welcome')
+        }
+        chrome.notifications.create('welcome', options);
+    }else if(details.reason == "update"){
+        var thisVersion = chrome.runtime.getManifest().version;
+        console.log("Updated from " + details.previousVersion + " to " + thisVersion + "!");
+        var options = {
+            type: "basic",
+            iconUrl: "https://leonekmi.twittolabel.fr/anilist-scrobble/update.png",
+            title: "Anilist Scrobbler",
+            message: chrome.i18n.getMessage('updated')
+        }
+        chrome.notifications.create('update', options);
     }
 });
