8be5d95659a2655d73d2f90a66ead80c252dd250
Leon <leonekmi@hotmail.com>
2017-12-12 20:30:47 +0100

    1.10
    Cache for multiple occurences

--- updater.js
+++ updater.js
@@ -1,25 +1,3 @@
-// Update Checker
-// This updater is discontinued, it will be deleted in 1.10
-/*var version = "1.9.4\n";
-$.get( {url: "https://leonekmi.twittolabel.fr/anilist-scrobble/version.txt", cache: false} , function (data) {
-    console.log("Version " + data);
-    if (version != data) {
-        console.log("Update available");
-        var options = {
-            type: "basic",
-            iconUrl: "https://leonekmi.twittolabel.fr/anilist-scrobble/update.png",
-            title: "Anilist Scrobbler",
-            message: chrome.i18n.getMessage('update')
-        }
-        chrome.notifications.create('update', options);
-        chrome.notifications.onClicked.addListener(function (notifId) {
-            chrome.tabs.create({ url: "https://leonekmi.twittolabel.fr/anilist-scrobble/update.html" });
-            chrome.notifications.clear(notifId);
-        });
-    } else {
-        console.log("No update available");
-    }
-});*/
 chrome.runtime.onInstalled.addListener(function(details) {
     if (details.reason == "install") {
         console.log("This is a first install!");
