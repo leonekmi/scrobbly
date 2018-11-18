b88ce75f87c56fd421190c19760a509f53060e85
leonekmi <leonekmi@hotmail.com>
2018-11-18 12:11:03 +0100

    License disclaimer

--- src/auth/anilist.js
+++ src/auth/anilist.js
@@ -1,3 +1,21 @@
+/*
+    This file is part of Scrobbly.
+
+    Scrobbly is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Scrobbly is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Scrobbly.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
 var $ = require('jquery');
 var browser = require('webextension-polyfill');
 var token = $('token').text();
--- src/pages/settings.js
+++ src/pages/settings.js
@@ -1,3 +1,20 @@
+/*
+    This file is part of Scrobbly.
+
+    Scrobbly is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Scrobbly is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Scrobbly.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 var $ = require('../Semantic-UI-CSS/semantic');
 var Vue = require('vue/dist/vue');
 var browser = require('webextension-polyfill');
